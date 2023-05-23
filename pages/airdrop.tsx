3e42921085310c5458cb33a67f4708c1a4f09ee0
BrimoZ <leagueofkalonline@gmail.com>
2023-05-24 02:02:26 +0300

    update

--- pages/airdrop.tsx
+++ pages/airdrop.tsx
@@ -11,7 +11,7 @@ export default function AirDrop() {
   return (
     <>
       <Head>
-        <title>Arbcity | AirDrop</title>
+        <title>Arbcityy | AirDrop</title>
         <meta name="description" content="Zap Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
